a4bd1db652888a3a989312ae95c32e9788e905a8
Alexey Kuzin <amkuzink@gmail.com>
2016-11-20 00:22:07 +0300

    Fins arc center coordinate (first working version)

--- guiapp/src/main/scala/choiceroulette/gui/ChoiceArc.scala
+++ guiapp/src/main/scala/choiceroulette/gui/ChoiceArc.scala
@@ -16,12 +16,15 @@
 
 package choiceroulette.gui
 
+import scala.language.implicitConversions
+import scalafx.beans.property.DoubleProperty
+import scalafx.geometry.Pos
 import scalafx.scene.Group
 import scalafx.scene.layout.StackPane
 import scalafx.scene.paint.Color
 import scalafx.scene.shape._
 import scalafx.scene.text.Text
-import scalafx.scene.transform.Rotate
+import scalafx.scene.transform.{Rotate, Translate}
 
 /**
   * @author Alexey Kuzin <amkuzink@gmail.com>
@@ -47,35 +50,59 @@ class ChoiceArc(radius: Double,
 
   private val mBackRectangle = Rectangle(2 * radius, 2 * radius, Color.Transparent)
 
-  private val mText = new Text("Choice") {
+  private val mText = new UpdatableText("Choice") {
     stroke = Color.Blue
   }
 
-  private def rotateText(text: Text): Unit = {
-    text.transforms.add(new Rotate(-(angleStart + angleLength / 2), radius, radius))
+  private def getArcCenter(arc: Arc): (Double, Double) = {
+    val arcStartAngleRad = math.toRadians(arc.startAngle)
+    val acrLenAngleRad = math.toRadians(arc.length)
+    val arcRadius: Double = arc.radiusX
+    val arcCenter: (Double, Double) = (arc.centerX, arc.centerY)
+
+    val p1 = getCirclePointCoordinates(arcCenter, arcRadius, arcStartAngleRad)
+    val p2 = getCirclePointCoordinates(arcCenter, arcRadius, arcStartAngleRad + acrLenAngleRad)
+
+    getArcCenter(arcCenter, p2, p1)
   }
 
-  private def getArcCenter(arc: Arc): (Double, Double) = {
-    val chordLen = 2 * arc.radiusX.value * math.sin(arc.length.value / 2)
-    val arcCenter = (arc.centerX.value, arc.centerY.value)
-    val arcRadius = arc.radiusX.value
-
-    val (x1, y1) = getCirclePointCoordinates(arcCenter, arcRadius, arc.startAngle.value)
-    val (x2, y2) = getCirclePointCoordinates(arcCenter, arcRadius, arc.startAngle.value + arc.length.value)
+  private def getArcCenter(p1: (Double, Double),
+                           p2: (Double, Double),
+                           p3: (Double, Double)): (Double, Double) = {
+    val x = (p1._1 + p2._1 + p3._1) / 3
+    val y = (p1._2 + p2._2 + p3._2) / 3
+    (x, y)
   }
 
   private def getCirclePointCoordinates(center: (Double, Double),
                                         radius: Double,
                                         angle: Double): (Double, Double) = {
     (center._1 + radius * math.cos(angle),
-      center._2 + radius * math.sin(angle))
+      center._2 - radius * math.sin(angle))
   }
 
-  rotateText(mText)
+  implicit def double2DoubleProperty(number: Double): DoubleProperty = DoubleProperty(number)
+  implicit def doubleProperty2Double(property: DoubleProperty): Double = property.value
 
-  children = List(new Group(mBackRectangle, mArc), mText)
+  mText.moveToArcCenter(mArc)
+  mText.rotateTextToArc(mArc)
+
+  children = new Group(mBackRectangle, mArc, mText)
   minHeight = 0
   minWidth = 0
   maxHeight = 2 * radius
   maxWidth = 2 * radius
+
+  private class UpdatableText(text: String) extends Text(text) { thisText =>
+
+    def moveToArcCenter(arc: Arc): Unit = {
+      val (x, y) = getArcCenter(arc)
+      relocate(x, y)
+    }
+
+    def rotateTextToArc(arc: Arc): Unit = {
+      val angle = -(arc.startAngle.value + arc.length.value / 2)
+      transforms.add(new Rotate(angle, 0, 0))
+    }
+  }
 }
